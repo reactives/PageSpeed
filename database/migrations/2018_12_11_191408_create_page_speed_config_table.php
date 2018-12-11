e1f52f116dbd430fa28c50172da08e5b0cad465a
Maksim Mysak <maksimmysak@gmail.com>
2018-12-11 21:29:58 +0200

    add phpdoc

--- database/migrations/2018_12_11_191408_create_page_speed_config_table.php
+++ database/migrations/2018_12_11_191408_create_page_speed_config_table.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+/**
+ * Class CreatePageSpeedConfigTable
+ */
 class CreatePageSpeedConfigTable extends Migration
 {
     const TABLE_NAME = 'page_speed_config';
