aaba10b89010cdb9d97929c58048fe180b1f8f34
Maksim Mysak <maksimmysak@gmail.com>
2018-12-11 21:27:35 +0200

    add page_speed_config table

---
+++ database/migrations/2018_12_11_191408_create_page_speed_config_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePageSpeedConfigTable extends Migration
+{
+    const TABLE_NAME = 'page_speed_config';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(self::TABLE_NAME, function (Blueprint $table) {
+            $table->string('id', 100)->primary();
+            $table->text('page_site_url')->nullable();
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+}
