63a6d0eba5bcd6e55792d77f414dd3b74978efa8
Maksim Mysak <maksimmysak@gmail.com>
2018-12-11 21:04:40 +0200

    add PageSpeedStatisticsController

---
+++ src/Http/Controllers/PageSpeedStatisticsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Reactives\PageSpeed\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class PageSpeedStatisticsController extends Controller
+{
+
+}
