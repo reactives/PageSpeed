ab9d4463f6f9017f17edfbf97e62ece4a810b563
Maksim Mysak <maksimmysak@gmail.com>
2018-12-11 21:38:05 +0200

    add models

---
+++ src/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Reactives\PageSpeed;
+
+use Illuminate\Database\Eloquent\Model as IlluminateModel;
+
+/**
+ * Class Model
+ *
+ * @package Reactives\PageSpeed
+ *
+ * @author Maksim Mysak <maksimmysak@gmail.com>
+ */
+abstract class Model extends IlluminateModel
+{
+}
---
+++ src/PageSpeedConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Reactives\PageSpeed;
+
+
+/**
+ * Class Model
+ *
+ * @package Reactives\PageSpeed
+ *
+ * @author Maksim Mysak <maksimmysak@gmail.com>
+ */
+class PageSpeedConfig extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'page_speed_config';
+}
